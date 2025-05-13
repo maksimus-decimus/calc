5b4d7c1062242288a6baa4919ca218ee1a4e8189
Maxi Fernandez <makushi2121@gmail.com>
2025-05-13 16:44:45 +0200

    calc

--- src/UI.java
+++ src/UI.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -26,10 +27,29 @@ public class UI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420, 640);
        
-       
-        JButton button = new JButton("Haz click aquí");
-        Dimension buttonSize = new Dimension(100, 30); // Set button size
+
+
+        ImageIcon originalIcon = new ImageIcon("src/boton1.png");
+        Image originalImage = originalIcon.getImage();
+        
+
+        // Scale the image to fit the button
+        int desiredWidth = 150;  // Set the desired width
+        int desiredHeight = 50; // Set the desired height
+        Image scaledImage = originalImage.getScaledInstance(desiredWidth, desiredHeight, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+
+        // Create a button with the scaled image
+        JButton button = new JButton(scaledIcon);
+        Dimension buttonSize = new Dimension(scaledIcon.getIconWidth(), scaledIcon.getIconHeight()); // Set button size to image size
         button.setPreferredSize(buttonSize);
+
+        button.setText(null);
+        button.setBorderPainted(false);
+        button.setFocusPainted(false);
+        button.setContentAreaFilled(false);
+
+
 
         int frameWidth = 420;  // Frame width
         int frameHeight = 640; // Frame height
@@ -40,9 +60,38 @@ public class UI {
 
         button.setBounds(x, y, buttonWidth, buttonHeight);
 
+        ImageIcon orImageIcon2 = new ImageIcon("src/boton2.png");
+        Image originalImage2 = orImageIcon2.getImage();
+        int desiredWidth2 = 150;  // Set the desired width
+        int desiredHeight2 = 50; // Set the desired height
+        Image scaledImage2 = originalImage2.getScaledInstance(desiredWidth2, desiredHeight2, Image.SCALE_SMOOTH);
+
+        ImageIcon scaledIcon2 = new ImageIcon(scaledImage2);
+        JButton button2 = new JButton(scaledIcon2);
+        Dimension buttonSize2 = new Dimension(scaledIcon2.getIconWidth(), scaledIcon2.getIconHeight()); // Set button size to image size
+        button2.setPreferredSize(buttonSize2);
+
+        button2.setText(null);
+        button2.setBorderPainted(false);
+        button2.setFocusPainted(false);
+        button2.setContentAreaFilled(false);
+
+        int frameWidth2 = 420;  // Frame width 
+        int frameHeight2 = 640; // Frame height
+        int buttonWidth2 = buttonSize2.width;
+        int buttonHeight2 = buttonSize2.height;
+        int x2 = (frameWidth2 - buttonWidth2) / 2;
+        int y2 = (frameHeight2 - buttonHeight2) / 2 + 100;
+
+        button2.setBounds(x2, y2, buttonWidth2, buttonHeight2);
+        
+        frame.add(button2);
         frame.add(button);
         frame.add(label);
         frame.setVisible(true);
+
+
+
 
         // Set the icon image
         ImageIcon image = new ImageIcon("src/logo.png");
